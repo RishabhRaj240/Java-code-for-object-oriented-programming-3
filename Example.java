c7844d602ebce91243c070f31a6d12220f702084
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-25 18:03:08 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 class Example {
-	int    i1 = 5;
-	double d1 = 6.7;
-	char   ch = 'a';
-	long[] la = {8, 9};
-	String s1 = "Hari";
+   int    i1 = 5;
+   double d1 = 6.7;
+   char   ch = 'a';
+   long[] la = {8, 9};
+   String s1 = "Hari";
 }
