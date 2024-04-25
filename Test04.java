2a58266a12b0509f5690a3aeea2c07cff21a4db6
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-25 18:04:16 +0530

    Update Test04.java

--- Test04.java
+++ Test04.java
@@ -1,17 +1,17 @@
 class Test04 {
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 
-		Example e1 = new Example();
+	Example e1 = new Example();
 
-		System.out.println(e1); //Example@2f92e0f4
+	System.out.println(e1); //Example@2f92e0f4
 
-		//Reading class Example values from the object e1
-		System.out.println(e1.i1); //5
-		System.out.println(e1.d1); //6.7
-		System.out.println(e1.ch); //a
-		System.out.println(e1.la); //[J@28a418fc
-		System.out.println(e1.la[0]); //8
-		System.out.println(e1.la[1]); //9
-		System.out.println(e1.s1); //Hari
-	}
+	//Reading class Example values from the object e1
+	System.out.println(e1.i1); //5
+	System.out.println(e1.d1); //6.7
+	System.out.println(e1.ch); //a
+	System.out.println(e1.la); //[J@28a418fc
+	System.out.println(e1.la[0]); //8
+	System.out.println(e1.la[1]); //9
+	System.out.println(e1.s1); //Hari
+   }
 }
